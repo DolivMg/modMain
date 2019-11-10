8f0b9773f9e58ba40f2b6248dc2aaeb249eacc82
TixonSc <coldsmile.1@gmail.com>
2019-11-10 15:28:23 +0200

    added: assets/ExampleMood/lang/en-ru.json assets/ExampleMood/models/HUI/HUI.json assets/ExampleMood/textures/HUI.png ... added texture, model and name for item "HUI".

---
+++ src/main/java/com/example/examplemod/Items.java
@@ -0,0 +1,36 @@
+package com.example.examplemod;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
+
+/**
+ * This class has the register event handler for all custom items.
+ * This class uses @Mod.EventBusSubscriber so the event handler has to be static
+ * This class uses @ObjectHolder to get a reference to the items
+ */
+@Mod.EventBusSubscriber(modid = ExampleMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@ObjectHolder(ExampleMod.MOD_ID)
+
+public class Items {
+
+    public static final Item HUI = null;
+
+    /**
+     * The actual event handler that registers the custom items.
+     *
+     * @param event The event this event handler handles
+     */
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {
+        //In here you pass in all item instances you want to register.
+        //Make sure you always set the registry name.
+        event.getRegistry().registerAll(
+
+                new Item(new Item.Properties()).setRegistryName(ExampleMod.MOD_ID, "HUI")
+        );
+    }
+
+}
